b14ff052bde895ddd32ff1c7e583dd27ff1cb98f
zivkoss <zivcoss@yahoo.com>
2023-07-25 18:55:29 +0200

    more code added 1977.171

--- js/app.js
+++ js/app.js
@@ -5,3 +5,34 @@ document.querySelector('#registracija').addEventListener('click', () => {
 document.querySelector('#closeModal').addEventListener('click', () => {
     document.querySelector('.custom-modal').style.display = 'none';
 });
+
+let config = {
+    'korisnicko_ime': {
+        required: true,
+        minlength: 5,
+        maxlength: 50
+    },
+
+    'register_email': {
+        required: true,
+        email: true,
+        minlength: 5,
+        maxlength: 50
+    },
+
+    'register_lozinka': {
+        required: true,
+        minlength: 7,
+        maxlength: 25,
+        matching: 'ponovi_lozinku'
+    },
+
+    'ponovi_lozinku': {
+        required: true,
+        minlength: 7,
+        maxlength: 50,
+        matching: 'register_lozinka'
+    }
+};
+
+let validator = new Validator(config, '#registrationForm');
